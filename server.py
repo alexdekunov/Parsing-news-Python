c817588f5b590469dbbb7b2645c7e5ff6a60758a
Dekunov Aleksandr <wresda@gmail.com>
2022-12-25 22:59:34 +0300

    chench

--- server.py
+++ server.py
@@ -7,11 +7,7 @@ app = Flask(__name__)
 def index():
     title = "прогноз погоды"
     weather = weather_by_city("Moscowweather_by_city,Russia")
-    if weather:
-        weather_text = f"Погода: {weather['temp_C']} градусов, ощущется как: {weather['FeelsLikeF']} градусов"
-    else:
-        weather_text = "Сервис погоды временно не доступен"
-    return render_template('index.html', page_title=title, weather=weather_text )
+    return render_template('index.html', page_title=title, weather=weather )
 
 if __name__ == "__main__":
     app.run(debug=True)
