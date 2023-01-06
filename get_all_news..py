6524ed6b7f1ccc07147e18dcf7a39c2843564c68
Dekunov Aleksandr <wresda@gmail.com>
2023-01-06 22:58:53 +0300

    добавлена база данных и вывод на сайт новостей с базы данных

---
+++ get_all_news..py
@@ -0,0 +1,8 @@
+# файл который будет собирать новости
+
+from webapp import create_app
+from webapp.python_org_news import get_python_news
+
+app = create_app()
+with app.app_context():
+    get_python_news()
